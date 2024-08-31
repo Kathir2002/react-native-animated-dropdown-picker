6049f7a51571da155587cba7b161b9363e3ac2fe
kathir <kathir@centiizen.com>
2024-08-31 12:43:31 +0530

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const appColors = {
+  primary: "#7F3DFF",
+  light: "#FFFFFF",
+  dark: "#212325",
+  formBorderColor: "#F1F1FA",
+  placeholderColor: "#91919F",
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as AnimatedDropdown } from "./dropdownPicker";
+export type {
+  ActivityIndicatorComponentPropsInterface,
+  ValueType,
+  ItemTypeValue,
+} from "./dropdownPicker";
